231aeb6f4176439983b469d97d8d04f5767bbd12
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2023-03-27 20:14:04 +0200

    dropdown for page about

---
+++ src/components/DropdownAbout.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const DropdownAbout = ({ title, desc }) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="dropdown-about">
+      <button className="btn" onClick={() => setOpen(!open)}>
+        {title}
+        <i
+          className={open ? 'fa-solid fa-angle-up' : 'fa-solid fa-angle-down'}
+        ></i>
+      </button>
+      {open && <p>{desc}</p>}
+    </div>
+  );
+};
+
+export default DropdownAbout;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DropdownAbout from '../components/DropdownAbout';
 import Footer from '../components/Footer';
 import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
+import { aboutData } from '../data/dataAbout';
 
 const About = () => {
+  const [data] = useState(aboutData);
   return (
     <>
       <div className="wrapper">
@@ -11,7 +14,17 @@ const About = () => {
           <Logo />
           <Navigation />
         </header>
-        <h1>about</h1>
+        <div className="about-content">
+          {data.map((quality, index) => {
+            return (
+              <DropdownAbout
+                key={index}
+                title={quality.title}
+                desc={quality.description}
+              />
+            );
+          })}
+        </div>
       </div>
       <Footer />
     </>
