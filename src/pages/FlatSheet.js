ede52a8704706ccec12e9fd8944033ee2fb4f146
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2023-03-27 12:18:50 +0200

    initialization react project & router

---
+++ src/pages/FlatSheet.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FlatSheet = () => {
+    return (
+        <div>
+           <h1>fiche d'appartement</h1> 
+        </div>
+    );
+};
+
+export default FlatSheet;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const Home = () => {
+const [data, setData]=useState([])
+
+
+
+    return (
+        <div>
+            <h1>home</h1>
+        </div>
+    );
+};
+
+export default Home;
