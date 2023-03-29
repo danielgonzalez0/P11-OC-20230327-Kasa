73132f4a40a8831756046dac12d60be77fe2dbeb
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2023-03-29 16:02:15 +0200

    flat page => start error handling

--- src/pages/FlatSheet.js
+++ src/pages/FlatSheet.js
@@ -5,88 +5,101 @@ import Footer from '../components/Footer';
 import Gallery from '../components/Gallery';
 import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
-
+import NotFound from './NotFound';
 
 const FlatSheet = () => {
   const [flatData, setFlatData] = useState({});
+  const [isError, setIsError] = useState(false);
   const location = useLocation();
 
   useEffect(() => {
     const { state } = location;
-    setFlatData(state.from);
+    if (state !== null) {
+      setFlatData(state.from);
+    } else {
+      setIsError(true);
+    }
   }, [location]);
 
   return (
     <>
-      {flatData && (
+      {isError ? (
+        <NotFound />
+      ) : (
         <>
-          <div className="wrapper" id="flat-page">
-            <header>
-              <Logo />
-              <Navigation />
-            </header>
-            <Gallery gallery={flatData.pictures} />
-            <div className="infos-container">
-              <div className="grid-container">
-                <h1>{flatData.title}</h1>
-                <p>{flatData.location}</p>
-                <div className="name-container">
-                  {flatData.host && (
-                    <>
-                      <span>{flatData.host.name}</span>
-                      <img
-                        src={flatData.host.picture}
-                        alt={flatData.host.name}
-                      />
-                    </>
-                  )}
-                </div>
+          {' '}
+          (
+          {flatData && (
+            <>
+              <div className="wrapper" id="flat-page">
+                <header>
+                  <Logo />
+                  <Navigation />
+                </header>
+                <Gallery gallery={flatData.pictures} />
+                <div className="infos-container">
+                  <div className="grid-container">
+                    <h1>{flatData.title}</h1>
+                    <p>{flatData.location}</p>
+                    <div className="name-container">
+                      {flatData.host && (
+                        <>
+                          <span>{flatData.host.name}</span>
+                          <img
+                            src={flatData.host.picture}
+                            alt={flatData.host.name}
+                          />
+                        </>
+                      )}
+                    </div>
 
-                <ul className="tag-list">
-                  {flatData.tags &&
-                    flatData.tags.map((tag, index) => (
-                      <li className="tag" key={index}>
-                        {tag}
+                    <ul className="tag-list">
+                      {flatData.tags &&
+                        flatData.tags.map((tag, index) => (
+                          <li className="tag" key={index}>
+                            {tag}
+                          </li>
+                        ))}
+                    </ul>
+                    <ul className="rating-list">
+                      <li>
+                        {flatData.rating >= 1 ? (
+                          <span className="fa-solid fa-star rating"></span>
+                        ) : (
+                          <span className="fa-sharp fa-solid fa-star not-rating"></span>
+                        )}
+                        {flatData.rating >= 2 ? (
+                          <span className="fa-sharp fa-solid fa-star rating"></span>
+                        ) : (
+                          <span className="fa-sharp fa-solid fa-star not-rating"></span>
+                        )}
+                        {flatData.rating >= 3 ? (
+                          <span className="fa-sharp fa-solid fa-star rating"></span>
+                        ) : (
+                          <span className="fa-sharp fa-solid fa-star not-rating"></span>
+                        )}
+                        {flatData.rating >= 4 ? (
+                          <span className="fa-sharp fa-solid fa-star rating"></span>
+                        ) : (
+                          <span className="fa-sharp fa-solid fa-star not-rating"></span>
+                        )}
+                        {flatData.rating >= 5 ? (
+                          <span className="fa-sharp fa-solid fa-star rating"></span>
+                        ) : (
+                          <span className="fa-sharp fa-solid fa-star not-rating"></span>
+                        )}
                       </li>
-                    ))}
-                </ul>
-                <ul className="rating-list">
-                  <li>
-                    {flatData.rating >= 1 ? (
-                      <span className="fa-solid fa-star rating"></span>
-                    ) : (
-                      <span className="fa-sharp fa-solid fa-star not-rating"></span>
-                    )}
-                    {flatData.rating >= 2 ? (
-                      <span className="fa-sharp fa-solid fa-star rating"></span>
-                    ) : (
-                      <span className="fa-sharp fa-solid fa-star not-rating"></span>
-                    )}
-                    {flatData.rating >= 3 ? (
-                      <span className="fa-sharp fa-solid fa-star rating"></span>
-                    ) : (
-                      <span className="fa-sharp fa-solid fa-star not-rating"></span>
-                    )}
-                    {flatData.rating >= 4 ? (
-                      <span className="fa-sharp fa-solid fa-star rating"></span>
-                    ) : (
-                      <span className="fa-sharp fa-solid fa-star not-rating"></span>
-                    )}
-                    {flatData.rating >= 5 ? (
-                      <span className="fa-sharp fa-solid fa-star rating"></span>
-                    ) : (
-                      <span className="fa-sharp fa-solid fa-star not-rating"></span>
-                    )}
-                  </li>
-                </ul>
+                    </ul>
+                  </div>
+                </div>
+                <div className="dropdown-container">
+                  <Dropdown title={'Description'} desc={flatData.description} />
+                  <Dropdown title={'Équipements'} desc={flatData.equipments} />
+                </div>
               </div>
-            </div>
-            <div className="dropdown-container">
-              <Dropdown title={'Description'} desc={flatData.description} />
-              <Dropdown title={'Équipements'} desc={flatData.equipments} />
-            </div>
-          </div>
-          <Footer />
+              <Footer />
+            </>
+          )}
         </>
       )}
     </>
