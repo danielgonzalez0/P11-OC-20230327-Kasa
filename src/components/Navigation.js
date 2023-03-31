c1bd8baa6cafcd5a7fed7d5cc13e7b4691e06742
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2023-03-31 18:46:43 +0200

    navigation add burger menu on mobile version

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
+  const [isActive, setActive] = useState('false');
+
+  const handleToggle = () => {
+    setActive(!isActive);
+  };
+
+  // btnSidebar.addEventListener('click', () => {
+  //   sidebar.classList.toggle('hidden');
+  // });
+
   return (
     <div className="navigation">
-      <ul>
-        <NavLink to="/" className={(nav) => (nav.isActive ? 'nav-active' : '')}>
-          <li>Accueil</li>
-        </NavLink>
-        
-        <NavLink
-          to="/about"
-          className={(nav) => (nav.isActive ? 'nav-active' : '')}
-        >
-          <li>A Propos</li>
-        </NavLink>
-      </ul>
+      <div
+        className={`toggle-btn ${!isActive ? 'show-cross' : ''}`}
+        onClick={handleToggle}
+      >
+        <span id="firstcrossbar"></span>
+        <span id="middlecrossbar"></span>
+        <span id="lastcrossbar"></span>
+      </div>
+      <div
+        id="side-bar"
+        className={`side-bar ${!isActive ? 'show-side-bar' : ''}`}
+      >
+        <ul>
+          <NavLink
+            to="/"
+            className={(nav) => (nav.isActive ? 'nav-active' : '')}
+          >
+            <li>Accueil</li>
+          </NavLink>
+
+          <NavLink
+            to="/about"
+            className={(nav) => (nav.isActive ? 'nav-active' : '')}
+          >
+            <li>A Propos</li>
+          </NavLink>
+        </ul>
+      </div>
     </div>
   );
 };
